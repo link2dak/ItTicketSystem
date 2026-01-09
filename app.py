f6775e41533b01ed306167a7f18be677a9e03cbd
Daniyal Khan <khan0838@umn.edu>
2026-01-09 15:35:44 -0600

    added backend logic

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 from flask import Flask,redirect,url_for,render_template,request
 
 app = Flask(__name__)
+lis = []
 
 @app.route('/')
 def home():
     # Render the HTML and pass data to it
     return render_template('index.html')
 
+@app.route('/result')
+def result():
+    #need to change so it shows currnet list
+    return render_template('result.html')
+
+
 
 @app.route('/submit', methods = ['POST', 'GET'])
 def submit():
     if request.method == 'POST':
-        print('working')
-    
+        lis.append({"name": request.form['name'],
+                    "email": request.form['email'],
+                    "department": request.form['department'],
+                    'priority': request.form['priority'],
+                    'subject': request.form['subject'],
+                    'description': request.form['description']})
+        
+    return redirect(url_for('result'))
 
 if __name__ == '__main__':
     app.run(debug = True)
