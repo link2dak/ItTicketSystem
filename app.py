d023cec8d9e67d628b20d7b231345267a67dd42f
Daniyal Khan <khan0838@umn.edu>
2026-01-20 01:17:06 -0600

    added content to buttons and put order to the list

--- app.py
+++ app.py
@@ -27,13 +27,41 @@ def submit():
     pdb.set_trace
 
     if request.method == 'POST':
-        lis.append({"name": request.form['name'],
-                    "email": request.form['email'],
-                    "department": request.form['department'],
-                    'priority': request.form['priority'],
-                    'subject': request.form['subject'],
-                    'description': request.form['description']})
+         dict = {"name": request.form['name'].capitalize(),
+                    "email": request.form['email'].capitalize(),
+                    "department": request.form['department'].capitalize(),
+                    'priority': request.form['priority'].capitalize(),
+                    'subject': request.form['subject'].capitalize(),
+                    'description': request.form['description'].capitalize()}
+         organize(dict)
+    
     return redirect(url_for('home'))
+
+# orginzes dictionary in order according to priority. High being at the top
+def organize(dict):
+    pdb.set_trace
+    i = len(lis) - 1
+    for dictlis in reversed(lis):
+        # If priority value is same, then put in list right after 
+        if prioityCheck(dict['priority']) == prioityCheck(dictlis['priority']):
+            lis.insert(i+1, dict)
+            return
+        # If higher put in list earlier
+        elif prioityCheck(dict['priority']) > prioityCheck(dictlis['priority']):
+            lis.insert(i, dict)
+            return                                                      
+        i = i+1
+    lis.append(dict)
+    
+        
+        
+def prioityCheck(priority):
+    if priority == "High":
+        return 3
+    elif priority == "Medium":
+        return 2
+    elif priority == 'Low':
+        return 1
 
 if __name__ == '__main__':
     app.run(debug = True)
